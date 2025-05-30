4515835f3c3ba8ade88ed66b19624850bd0f1b4e
Emilen096 <emiltsvetkov07@gmail.com>
2025-05-30 19:29:20 +0300

    Tetrahedron.h

--- Tetrahedron.h
+++ Tetrahedron.h
@@ -1,14 +1,19 @@
 #pragma once
 #include "Point.h"
+#include <cmath>
+#include <stdexcept>
 
 class Tetrahedron {
 private:
     Point a, b, c, d;
 
 public:
-    // Конструктор
+    // Конструктор с проверкой
     Tetrahedron(const Point& a, const Point& b, const Point& c, const Point& d);
-
+    
+    // Проверка валидности тетраэдра
+    bool isValid(double tolerance = 1e-9) const;
+    
     // Метод для расчёта объёма
     double volume() const;
 };
