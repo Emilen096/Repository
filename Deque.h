822d540c9c5aa0f4dde70c91b64aa379d05df6f3
Emilen096 <emiltsvetkov07@gmail.com>
2025-05-31 10:25:54 +0300

    Deque.h

--- Deque.h
+++ Deque.h
@@ -2,30 +2,41 @@
 #include <vector>
 #include <string>
 #include <initializer_list>
+#include <iostream>
 
 class Deque {
 public:
-    Deque();
+    // Конструкторы
+    Deque() = default;
     Deque(std::initializer_list<int> init);
-    Deque(const Deque& other);
-    Deque(Deque&& other) noexcept;
-    ~Deque();
+    Deque(const Deque& other) = default;
+    Deque(Deque&& other) noexcept = default;
+    
+    // Операторы присваивания
+    Deque& operator=(const Deque& other) = default;
+    Deque& operator=(Deque&& other) noexcept = default;
+    
+    // Деструктор
+    ~Deque() = default;
 
-    Deque& operator=(const Deque& other);
-    Deque& operator=(Deque&& other) noexcept;
-
+    // Основные операции
     void push_back(int value);
     void push_front(int value);
     void pop_back();
     void pop_front();
 
+    // Доступ к элементам
     int front() const;
     int back() const;
 
-    bool empty() const;
-    size_t size() const;
+    // Состояние дека
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+    
+    // Преобразование в строку
     std::string toString() const;
 
+    // Операторы сдвига
     friend std::ostream& operator<<(std::ostream& os, const Deque& deque);
     friend std::istream& operator>>(std::istream& is, Deque& deque);
 
