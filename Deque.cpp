25dd18f47ac538cc631094ca3039780e2ae0c2af
Emilen096 <emiltsvetkov07@gmail.com>
2025-05-31 10:26:06 +0300

    Deque.cpp

--- Deque.cpp
+++ Deque.cpp
@@ -1,91 +1,93 @@
 #include "Deque.h"
 #include <sstream>
+#include <stdexcept>
 
-Deque::Deque() = default;
-
+// Конструктор со списком инициализации
 Deque::Deque(std::initializer_list<int> init) {
     for (int val : init) {
         data.push_back(val);
     }
 }
 
-Deque::Deque(const Deque& other) : data(other.data) {}
-
-Deque::Deque(Deque&& other) noexcept : data(std::move(other.data)) {}
-
-Deque::~Deque() = default;
-
-Deque& Deque::operator=(const Deque& other) {
-    if (this != &other) {
-        data = other.data;
-    }
-    return *this;
-}
-
-Deque& Deque::operator=(Deque&& other) noexcept {
-    if (this != &other) {
-        data = std::move(other.data);
-    }
-    return *this;
-}
-
+// Добавление в конец
 void Deque::push_back(int value) {
     data.push_back(value);
 }
 
+// Добавление в начало
 void Deque::push_front(int value) {
     data.insert(data.begin(), value);
 }
 
+// Удаление с конца
 void Deque::pop_back() {
-    if (!data.empty()) {
-        data.pop_back();
+    if (data.empty()) {
+        throw std::out_of_range("Deque is empty");
     }
+    data.pop_back();
 }
 
+// Удаление с начала
 void Deque::pop_front() {
-    if (!data.empty()) {
-        data.erase(data.begin());
+    if (data.empty()) {
+        throw std::out_of_range("Deque is empty");
     }
+    data.erase(data.begin());
 }
 
+// Первый элемент
 int Deque::front() const {
-    if (data.empty()) throw std::out_of_range("Deque is empty");
+    if (data.empty()) {
+        throw std::out_of_range("Deque is empty");
+    }
     return data.front();
 }
 
+// Последний элемент
 int Deque::back() const {
-    if (data.empty()) throw std::out_of_range("Deque is empty");
+    if (data.empty()) {
+        throw std::out_of_range("Deque is empty");
+    }
     return data.back();
 }
 
-bool Deque::empty() const {
+// Проверка на пустоту
+bool Deque::empty() const noexcept {
     return data.empty();
 }
 
-size_t Deque::size() const {
+// Размер дека
+size_t Deque::size() const noexcept {
     return data.size();
 }
 
+// Преобразование в строку
 std::string Deque::toString() const {
     std::ostringstream oss;
-    oss << "[ ";
-    for (int val : data) {
-        oss << val << " ";
+    oss << "[";
+    for (size_t i = 0; i < data.size(); ++i) {
+        oss << (i > 0 ? " " : "") << data[i];
     }
     oss << "]";
     return oss.str();
 }
 
+// Оператор вывода (сдвиг влево)
 std::ostream& operator<<(std::ostream& os, const Deque& deque) {
     os << deque.toString();
     return os;
 }
 
+// Оператор ввода (сдвиг вправо)
 std::istream& operator>>(std::istream& is, Deque& deque) {
-    int val;
-    while (is >> val) {
-        deque.push_back(val);
+    int value;
+    while (is >> value) {
+        deque.push_back(value);
+        
+        // Проверка следующего символа
+        if (is.peek() == '\n' || is.peek() == EOF) {
+            break;
+        }
     }
     return is;
 }
