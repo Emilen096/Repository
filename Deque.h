10c18d1886e7134546a46c95ca2a5e8f73c29a37
Emilen096 <emiltsvetkov07@gmail.com>
2025-06-06 13:39:57 +0300

    Deque.h

--- Deque.h
+++ Deque.h
@@ -1,17 +1,21 @@
 #pragma once
 #include <cstddef>
-#include <string>
 #include <initializer_list>
-#include <ostream>
-#include <istream>
+#include <string>
+#include <iostream>
 
 class Deque {
+private:
+    static const std::size_t capacity = 100;  // Статическое максимальное значение
+    int data[capacity];                       // Статический массив
+    std::size_t count;                        // Количество элементов
+
 public:
     Deque();
     Deque(std::initializer_list<int> init);
     Deque(const Deque& other);
     Deque(Deque&& other) noexcept;
-    ~Deque();
+    ~Deque() = default;
 
     Deque& operator=(const Deque& other);
     Deque& operator=(Deque&& other) noexcept;
@@ -23,21 +27,11 @@ public:
 
     int front() const;
     int back() const;
-
     bool empty() const;
     std::size_t size() const;
+
     std::string toString() const;
 
     friend std::ostream& operator<<(std::ostream& os, const Deque& deque);
     friend std::istream& operator>>(std::istream& is, Deque& deque);
-
-private:
-    int*         data;      // Указатель на динамически выделенный массив элементов
-    std::size_t  capacity;  // Максимально допустимое количество элементов
-    std::size_t  count;     // Текущее количество элементов в деке
-    std::size_t  head;      // Индекс в массиве, где находится первый (front) элемент
-    std::size_t  tail;      // Индекс “за” последним (back) элементом; invariant: tail = (head + count) % capacity
-
-    void resizeBuffer();
-
 };
